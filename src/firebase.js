b8b9b47ea0ba0af03b2663532115c0ae59108ced
Bina Khalid <binaanwar@gmail.com>
2017-10-03 22:15:32 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config  = {
+    apiKey: "AIzaSyBadd0t-gHd_W6aq9x2iNBbjQy9PiVxtAY",
+    authDomain: "goalcoach-8be0f.firebaseapp.com",
+    databaseURL: "https://goalcoach-8be0f.firebaseio.com",
+    projectId: "goalcoach-8be0f",
+    storageBucket: "goalcoach-8be0f.appspot.com",
+    messagingSenderId: "945859078866"
+  };
+
+   export const firebaseApp = firebase.initializeApp(config);
